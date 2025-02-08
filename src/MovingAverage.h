cc48bbf2213a64013f1efb89a68931b04f261c17
me <me>
2025-02-08 11:07:26 +0300

    Добавлено определение функции скользящего среднего

--- src/MovingAverage.h
+++ src/MovingAverage.h
@@ -1,4 +1,19 @@
 #ifndef MOVINGAVERAGE_H
 #define MOVINGAVERAGE_H
 
+#include <vector>
+#include <numeric>
+
+typedef enum {
+    completed,
+    in_data_empty,
+    window_size_invalid,
+} status;
+
+
+template <typename T>
+status moving_average(const std::vector<T>&in_data, std::vector<T>&out_data, const int window_size)
+{
+    return completed;
+}
 #endif // MOVINGAVERAGE_H
